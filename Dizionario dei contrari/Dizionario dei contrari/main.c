c1d53f2ecab1551184e314bb27542dc5bce6c828
Roberto Vecchio <info@robertovecchio.com>
2019-02-03 13:38:48 +0100

    Aggiunto main
    - aggiunto e inizializzato file main.c

---
+++ Dizionario dei contrari/Dizionario dei contrari/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Dizionario dei contrari
+//
+//  Created by Roberto Vecchio on 03/02/2019.
+//  Copyright © 2019 Roberto Vecchio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
